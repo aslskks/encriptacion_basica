65815fd3c7520b8a36c1ff3b617d119570c21d5d
aslskks <130728002+aslskks@users.noreply.github.com>
2023-09-17 09:36:50 -0600

    code

--- desencriptar.py
+++ desencriptar.py
@@ -1,8 +1,1 @@
-import getpass
-import base64
-from cryptography.fernet import Fernet
-datos_desencriptados = fernet.decrypt(datos_encriptados)
-archivo_original = archivo_encriptado.replace('.encrypted', '')
-with open(archivo_original, 'wb') as archivo_destino:
-    archivo_destino.write(datos_desencriptados)
-print(f"El archivo {archivo_encriptado} se ha desencriptado correctamente como {archivo_original}.")
+print("no se puedo perdon")
